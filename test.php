2df434b2d0edca586c876d1e49b4dd0a45ae42e0
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2024-03-07 11:01:41 +0100

    Update test.php

--- test.php
+++ test.php
@@ -9,3 +9,7 @@ while ($tab3=mysqli_fetch_array($wynik3) )
     echo '<tr><td style="border: 1px solid black; border-collapse: collapse;">".$tab3['Id_klienta']."</td><td>".$tab3['Nazwisko']."</td><td>".$tab3['Imie']."</td><td>".$tab3['Miejscowosc']."</td></tr>';
 
 }
+
+while ($tab3=mysqli_fetch_array($wynik3)) {
+    echo '<tr><td style="border: 1px solid black; border-collapse: collapse;">'.$tab3['Id_klienta'].'</td><td>'.$tab3['Nazwisko'].'</td><td>'.$tab3['Imie'].'</td><td>'.$tab3['Miejscowosc'].'</td></tr>';
+}
