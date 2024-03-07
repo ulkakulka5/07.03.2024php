1ea646d4439791d0d68ececfb0775bfc501a8d9f
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2024-03-07 11:01:00 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 echo "<table style="border: 1px solid black; border-collapse: collapse;">";
 
 echo '<table style="border: 1px solid black; border-collapse: collapse;">';
+
+
+while ($tab3=mysqli_fetch_array($wynik3) )
+{
+    
+    echo '<tr><td style="border: 1px solid black; border-collapse: collapse;">".$tab3['Id_klienta']."</td><td>".$tab3['Nazwisko']."</td><td>".$tab3['Imie']."</td><td>".$tab3['Miejscowosc']."</td></tr>';
+
+}
